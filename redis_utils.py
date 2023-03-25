c388f0be94a8a55034a8555db9df14002ecde61e
Adam Yedidia <adamyedidia@gmail.com>
2023-03-25 18:01:16 -0400

    Fewer walruses

--- redis_utils.py
+++ redis_utils.py
@@ -4,7 +4,8 @@ from typing import Optional, Any
 redis = r.Redis(connection_pool=r.ConnectionPool(host='localhost', port=6379, db=0))
 
 def rget(key: str, *, game_id: str) -> Optional[str]:
-    return raw_result.decode('utf-8') if (raw_result := redis.get(f'{game_id}:{key}')) is not None else None
+    raw_result = redis.get(f'{game_id}:{key}')
+    return raw_result.decode('utf-8') if raw_result is not None else None
 
 def rset(key: str, value: Any, *, game_id: str) -> None:
     redis.set(f'{game_id}:{key}', value)
